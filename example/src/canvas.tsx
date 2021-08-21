f59261d713cdea1ca7e00317f0864cd4e8afefce
joshmarinacci <joshua@marinacci.org>
2021-08-20 21:17:35 -0700

    work on a backdrop

--- example/src/canvas.tsx
+++ example/src/canvas.tsx
@@ -50,6 +50,7 @@ class Rect {
   }
 }
 
+// @ts-ignore
 function calc_scene_bounds(provider: TreeItemProvider):Rect {
   let bounds = new Rect(0,0,0,0)
   bounds.x = 0
@@ -65,16 +66,15 @@ function calc_scene_bounds(provider: TreeItemProvider):Rect {
   return bounds
 }
 
-function draw_to_canvas(can: HTMLCanvasElement, provider:TreeItemProvider, scale: number, selMan: SelectionManager, bounds:Rect) {
+function draw_to_canvas(can: HTMLCanvasElement, provider:TreeItemProvider,
+                        scale: number, selMan: SelectionManager,
+                        bounds:Rect, page:Rect) {
   const c = can.getContext('2d') as CanvasRenderingContext2D
-  c.fillStyle = 'white'
-  c.fillRect(0, 0, bounds.x2, bounds.y2)
+  bounds.fill(c,'#cccccc')
   c.save()
   c.scale(scale,scale)
   c.translate(-bounds.x,-bounds.y)
-
-  bounds.fill(c,'cyan')
-
+  page.fill(c,'white')
   provider.getSceneRoot().children.forEach((ch:any) => {
     c.fillStyle = ch.color
     c.fillRect(ch.x,ch.y,ch.w,ch.h)
@@ -94,7 +94,6 @@ function draw_to_canvas(can: HTMLCanvasElement, provider:TreeItemProvider, scale
 function canvas_to_point(e: MouseEvent, scale:number):Point {
   // @ts-ignore
   let rect = e.target.getBoundingClientRect()
-  // let rect = e.target.getClientBounds()
   return new Point(e.clientX-rect.x, e.clientY-rect.y).divide(scale)
 }
 
@@ -142,12 +141,13 @@ export function RectCanvas(props:{provider:TreeItemProvider}) {
     if(!canvas.current) return
     // @ts-ignore
     let can = canvas.current as HTMLCanvasElement
-    let bds = calc_scene_bounds(props.provider)
+    let scene_bounds = calc_scene_bounds(props.provider)
+    let bds = new Rect(0,0,can.clientWidth, can.clientHeight)
     if(!bds.equal(bounds)) {
       set_bounds(bds)
       set_count(count+1)
     } else {
-      draw_to_canvas(can, props.provider, scale, selMan, bds)
+      draw_to_canvas(can, props.provider, scale, selMan, bds, scene_bounds)
     }
   }
 
@@ -182,13 +182,12 @@ export function RectCanvas(props:{provider:TreeItemProvider}) {
       })
     }
   }
-
   const mouseUp = (e:MouseEvent<HTMLCanvasElement>) => {
     set_mouse_pressed(false)
     set_mouse_start(new Point(0,0))
   }
 
-  const contextMenu = (e:MouseEvent<HTMLCanvasElement>) => {
+  const showContextMenu = (e:MouseEvent<HTMLCanvasElement>) => {
     e.preventDefault()
     e.stopPropagation()
     set_mouse_pressed(false)
@@ -232,16 +231,14 @@ export function RectCanvas(props:{provider:TreeItemProvider}) {
       pm.show(<ContextMenu menu={menu} />, e.target, pt)
     }
   }
-  // let bds = calc_scene_bounds(props.provider)
-  let w = bounds.width()
-  let h = bounds.height()
   return <div className="panel">
-    <canvas style={{border: '1px solid red', width:`${w}px`, height:`${h}px`}}
-    width={w} height={h} ref={canvas}
+    <canvas style={{border: '1px solid red', width:`100%`, height:`100%`}}
+      width={bounds.width()} height={bounds.height()}
+      ref={canvas}
       onMouseDown={mouseDown}
       onMouseMove={mouseMove}
       onMouseUp={mouseUp}
-            onContextMenu={contextMenu}
+      onContextMenu={showContextMenu}
     />
   </div>
 }
